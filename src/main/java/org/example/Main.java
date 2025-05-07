675ca5f238f0646f99fab893cdf24e79fe50b785
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-07 14:50:20 +0300

    created person objects and methods tested

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person1 = new Person("John", "Doe", 20);
+        Person person2 = new Person("Alice", "Smith", 16, "Female", "alice@example.com", true);
+
+        System.out.println("Person 1:");
+        System.out.println("Firstname: " + person1.getFirstName());
+        System.out.println("Lastname: " + person1.getLastName());
+        System.out.println("Age: " + person1.getAge());
+        System.out.println("Is Teen? " + person1.isTeen());
+
+        System.out.println();
+
+        System.out.println("Person 2:");
+        System.out.println("Firstname: " + person2.getFirstName());
+        System.out.println("Lastname: " + person2.getLastName());
+        System.out.println("Age: " + person2.getAge());
+        System.out.println("Is Teen? " + person2.isTeen());
     }
+
 }
