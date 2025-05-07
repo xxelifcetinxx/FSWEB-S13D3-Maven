a22d5e8ccdf16c35d272fda7ca989b02dff2e34a
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-07 14:41:26 +0300

    added 2 constructor

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -4,12 +4,23 @@ public class Person {
     private String firstName;
     private String lastName;
     private int age;
-
-
     // Ekstra özellikler
     private String gender;
     private String email;
     private boolean isEmployed;
 
+    // Constructor 1
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 
+    // Constructor 2 - Overloading + Chaining
+    public Person(String firstName, String lastName, int age, String gender, String email, boolean isEmployed) {
+        this(firstName, lastName, age); // constructor chaining
+        this.gender = gender;
+        this.email = email;
+        this.isEmployed = isEmployed;
+    }
 }
