6a021889cfa39ed237c822301a9be5c2562419f7
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-07 14:56:01 +0300

    getter and setter methods added in wall class

--- src/main/java/org/example/Wall.java
+++ src/main/java/org/example/Wall.java
@@ -8,4 +8,28 @@ public class Wall {
         this.width = width;
         this.height = height;
     }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
 }
