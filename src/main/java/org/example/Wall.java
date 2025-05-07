f3078768e25aa14b791b20fd8d1bb57bec4b64fd
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-07 14:54:30 +0300

    constructor added in wall class

--- src/main/java/org/example/Wall.java
+++ src/main/java/org/example/Wall.java
@@ -3,4 +3,9 @@ package org.example;
 public class Wall {
     private double width;
     private double height;
+
+    public Wall(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
 }
